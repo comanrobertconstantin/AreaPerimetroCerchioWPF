e631c2b4de894f15795c6aca8c0f4324edcba34d
Robert Constantin Coman <coman.robert.constantin@gmail.com>
2020-03-26 17:44:51 +0100

    codice metodo btnArea

--- AreaPerimetroCerchioConsoleApp/WpfAreaPerimetroCerchio/MainWindow.xaml.cs
+++ AreaPerimetroCerchioConsoleApp/WpfAreaPerimetroCerchio/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,25 @@ namespace WpfAreaPerimetroCerchio
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void btnArea_Click(object sender, RoutedEventArgs e)
+        {
+            double ris = 0;
+
+            double raggio = Convert.ToDouble(txtNr.Text);
+
+            HttpClient client = new HttpClient();
+
+            string url = "https://localhost:44327/api/Operations/GetArea?raggio={raggio}";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            ris = JsonConvert.DeserializeObject<double>(content);
+
+            Dispatcher.Invoke(() => lblArea.Content = ris);
         }
     }
 }
