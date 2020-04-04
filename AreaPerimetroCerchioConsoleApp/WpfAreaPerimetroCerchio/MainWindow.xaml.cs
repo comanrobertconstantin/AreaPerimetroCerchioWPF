192adaa661374e0d36c87add449378ae6482698a
Robert Constantin Coman <coman.robert.constantin@gmail.com>
2020-04-04 19:09:49 +0200

    aggiornamento codice

--- AreaPerimetroCerchioConsoleApp/WpfAreaPerimetroCerchio/MainWindow.xaml.cs
+++ AreaPerimetroCerchioConsoleApp/WpfAreaPerimetroCerchio/MainWindow.xaml.cs
@@ -27,28 +27,41 @@ namespace WpfAreaPerimetroCerchio
             InitializeComponent();
         }
 
-        private async void btnAreaAndPerimeter_Click(object sender, RoutedEventArgs e)
+        private async void btnArea_Click(object sender, RoutedEventArgs e)
         {
             double risArea = 0;
+
+            double raggio = Convert.ToDouble(txtRadius.Text);
+
+            HttpClient client = new HttpClient();
+
+            string urlArea = $"https://localhost:44327/api/Operations/GetArea?raggio={raggio}";
+
+            HttpResponseMessage responseArea = await client.GetAsync(urlArea);
+
+            string contentArea = await responseArea.Content.ReadAsStringAsync();
+
+            risArea = JsonConvert.DeserializeObject<double>(contentArea);
+
+            Dispatcher.Invoke(() => lblArea.Content = risArea);
+        }
+
+        private async void btnPerimeter_Click(object sender, RoutedEventArgs e)
+        {
             double risPerimeter = 0;
 
             double raggio = Convert.ToDouble(txtRadius.Text);
 
             HttpClient client = new HttpClient();
 
-            string urlArea = "https://localhost:44327/api/Operations/GetArea?raggio={raggio}";
-            string urlPerimeter = "https://localhost:44327/api/Operations/GetPerimeter?raggio={raggio}";
+            string urlPerimeter = $"https://localhost:44327/api/Operations/GetPerimeter?raggio={raggio}";
 
-            HttpResponseMessage responseArea = await client.GetAsync(urlArea);
             HttpResponseMessage responsePerimeter = await client.GetAsync(urlPerimeter);
 
-            string contentArea = await responseArea.Content.ReadAsStringAsync();
             string contentPerimeter = await responsePerimeter.Content.ReadAsStringAsync();
 
-            risArea = JsonConvert.DeserializeObject<double>(contentArea);
             risPerimeter = JsonConvert.DeserializeObject<double>(contentPerimeter);
 
-            Dispatcher.Invoke(() => lblArea.Content = risArea);
             Dispatcher.Invoke(() => lblPerimeter.Content = risPerimeter);
         }
     }
